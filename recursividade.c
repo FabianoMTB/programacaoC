17c62012562b2e8a55c820a1548ff5afb05e9ab1
FabianoMTB <rockfabianomedeiros@gmail.com>
2025-08-24 00:44:21 +0000

    contagem regressiva

---
+++ recursividade.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+    void recursivo(int numero){
+       if (numero > 0){
+        printf("%d \n", numero);
+
+        recursivo(numero -1);
+       
+        /* code */
+       }
+       
+    }
+int main (){
+
+   int quantidade = 10;
+   
+   printf ("Contagem regresiva...");
+   recursivo (quantidade);
+       
+    return 0;
+}
